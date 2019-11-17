3453366e7b28d0ae3b533d0f3535b9ae0d45b767
teopinillo <teopinillo@gmail.com>
2019-11-16 23:00:14 -0500

    Updating gradle

--- app/src/main/java/com/teopinillo/friendlychat/MainActivity.java
+++ app/src/main/java/com/teopinillo/friendlychat/MainActivity.java
@@ -1,10 +1,15 @@
 package com.teopinillo.friendlychat;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.preference.PreferenceManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -15,14 +20,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
+import com.google.android.gms.auth.api.Auth;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
     private RecyclerView recyclerView;
     private MessageAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
@@ -33,6 +41,19 @@ public class MainActivity extends AppCompatActivity {
     private Button btSend;
     private String username = "Anonymus";
 
+    private static final String TAG = "MainActivity";
+    public static final String MESSAGES_CHILD = "messages";
+    private static final int REQUEST_INVITE = 1;
+    private static final int REQUEST_IMAGE = 2;
+    private static final String LOADING_IMAGE_URL = "https://www.google.com/images/spin-32.gif";
+    public static final int DEFAULT_MSG_LENGTH_LIMIT = 10;
+    private static final String MESSAGE_SENT_EVENT = "message_sent";
+    private String mUsername;
+    private String mPhotoUrl;
+    private SharedPreferences mSharedPreferences;
+    private GoogleApiClient mGoogleApiClient;
+    private static final String MESSAGE_URL = "http://friendlychat.firebase.google.com/message/";
+
     //TO be able to use databse we need to declare this
     //two classes.
     private FirebaseDatabase firebaseDatabase;
@@ -42,6 +63,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        //set default username as anonymous
+        mUsername = "Anonymous";
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API)
+                .build();
 
         //instance the database objects
         firebaseDatabase = FirebaseDatabase.getInstance();
@@ -109,6 +139,8 @@ public class MainActivity extends AppCompatActivity {
                 //Send message
                 //Creating the message
                 Message message = new Message( username, etMessage.getText().toString(),"" );
+                myDataset.add (message);
+                mAdapter.notifyDataSetChanged();
                 //clear input box
                 etMessage.setText("");
             }
@@ -128,5 +160,13 @@ public class MainActivity extends AppCompatActivity {
         myDataset.add (new Message("Hola","Ana",""));
         myDataset.add (new Message("Good","Bigui",""));
 
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
     }
 }
